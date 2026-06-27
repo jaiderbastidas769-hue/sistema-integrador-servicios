a81fefdf3f1925d105053e5d99d82aeae0161e80
jaiderbastidas769-hue <jaiderbastidas769@gmail.com>
2026-06-27 17:13:51 -0500

    Update 02_integracion_ventas.sql

--- 02_integracion_ventas.sql
+++ 02_integracion_ventas.sql
@@ -1,1 +1,21 @@
+USE smart_stock_db;
 
+-- Tabla de Ventas
+CREATE TABLE IF NOT EXISTS ventas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_venta DECIMAL(12, 2) NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+-- Detalle de la Venta (Integración lógica entre ventas y productos)
+CREATE TABLE IF NOT EXISTS detalle_ventas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    venta_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_subtotal DECIMAL(12, 2) NOT NULL,
+    FOREIGN KEY (venta_id) REFERENCES ventas(id) ON DELETE CASCADE,
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
